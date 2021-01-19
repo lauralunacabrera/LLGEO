cf520ae4d9edddbe4e5ee0d56c87cae9c3db3d15
laura <lauralunacabrera@gmail.com>
2021-01-19 16:28:53 -0400

    selenium trials

--- tests/temporary/selenium_trial.py
+++ tests/temporary/selenium_trial.py
@@ -1,26 +1,56 @@
-#%%
-
+#%%-----------------------------------------------------------------------------
+# Modules - requires install of Firefox webdrivers
+# ------------------------------------------------------------------------------
 import time
+from bs4 import BeautifulSoup
 from datetime import datetime
 from selenium import webdriver
-
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait as dwait
 from selenium.webdriver.support import expected_conditions as ec
 
-from bs4 import BeautifulSoup
+# ------------------------------------------------------------------------------
+# User-Facing Functions
+# ------------------------------------------------------------------------------
+def find_gym_times(username, password, target_time):
+
+    # Login to dalplex
+    driver = login_dalplex(username, password)
+
+    # Click on gym buttom and sleep for 2 seconds
+    gym_xpath  = '/html/body/div[6]/div[2]/div[2]/div/div[2]/div/div/div/'
+    gym_xpath += 'div[25]/div/div[2]/div[1]/h4'
+    gym_button = get_xelem(driver, gym_xpath)
+    driver.execute_script("arguments[0].click()", gym_button)
+    time.sleep(2)
+
+    # Find scheduling cards
+    cards_xpath = '//div[@class="caption program-schedule-card-caption"]'
+    cards = driver.find_elements_by_xpath(cards_xpath)
+
+    # Available times
+    times, spots = parse_schedule_cards(cards)
+
+    # TODO - continue here
+    # Find closest availale time
+    # deltas = [datetime.timedtarget_time - avail_time for avail_time in times]
+    # deltas
+
+    return(cards)
 
 
-# %%
-
+# ------------------------------------------------------------------------------
+# Helper Functions
+# ------------------------------------------------------------------------------
 def get_xelem(driver, xpath, t = 10):
+    ''' Returns element from x_path, waiting until it is present'''
     source = (By.XPATH, xpath)
     element = dwait(driver, t).until(ec.presence_of_element_located(source))
     return element
 
 
 def login_dalplex(username, password):
-    ''' logins to dalplex and returns firefox drivers '''
+    ''' Logs in to dalplex and returns firefox drivers '''
 
     # Specify xpaths to website objects
     lgl_xpath = '//*[@id="loginLink"]'
@@ -62,8 +92,7 @@ def parse_schedule_cards(cards):
     ''' Returns available times when given scheduling cards '''
 
     clean = lambda str_in : str_in.replace('\n','').replace('\t','')
-    times = []
-    spots = []
+    times, spots = [], []
 
     for card in cards:    
         # Initialize HTML parser
@@ -85,40 +114,12 @@ def parse_schedule_cards(cards):
 
     return(times, spots)
 
-def match_schedule_cards(target_time, cards):
 
-    # Get datetime and spots from cards
-    avail_times, spots = parse_schedule_cards(cards)
-
-
-
-def find_gym_times(username, password, target_time):
-
-    # Login to dalplex
-    driver = login_dalplex(username, password)
-
-    # Click on gym buttom and sleep for 2 seconds
-    gym_xpath  = '/html/body/div[6]/div[2]/div[2]/div/div[2]'
-    gym_xpath += '/div/div/div/div[24]/div'
-    gym_button = get_xelem(driver, gym_xpath)
-    driver.execute_script("arguments[0].click()", gym_button)
-    time.sleep(2)
-
-    # Find scheduling cards
-    cards_xpath = '//div[@class="caption program-schedule-card-caption"]'
-    cards = driver.find_elements_by_xpath(cards_xpath)
-
-    # Available times
-    times, spots = parse_schedule_cards(cards)
-
-    # Find closest availale time
-    deltas = [datetime.timedtarget_time - avail_time for avail_time in times]
-    deltas
-
-    return(times, spots)
-
-
+# ------------------------------------------------------------------------------
+# Example
+# ------------------------------------------------------------------------------
 username = 'lr495010'
 password = 'Lala362636'
+find_gym_times(username, password, None)
 
 # %%
